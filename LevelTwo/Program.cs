027c8cadcf342278f1e9e7e376fc717079088a15
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-03-20 18:32:09 +0000

    Exercicio 2

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {   
+            if(args.Length>0)
+            {
+            string nome =args[0];
+            CanvasImage image=new CanvasImage(nome);
+
+            int tamanho=int.Parse(args[1]);
+
+            image.MaxWidth(tamanho);
+
+
+            AnsiConsole.Write(image);
+            }
+            else
+            {
+            
+            CanvasImage image=new CanvasImage("tux.jpg");
+
+           
+
+            image.MaxWidth(24);
+
+
+            AnsiConsole.Write(image);
+            }        
+        
+        }
+
+    }
+}
